ed590220b370b678de6b40986d3d9c3db6642dfc
Sathavaram Lahare <142076496+sathavaramlahare@users.noreply.github.com>
2026-07-09 21:41:04 +0530

    Update uart.h

--- Header_Files/uart.h
+++ Header_Files/uart.h
@@ -2,8 +2,6 @@ void INIT_UART(void);
 void U0_TXBYTE(char SBYTE);
 void U0_TX_BYTE_string(char *SBYTE);
 void U0_TX_BYTE_int(int SBYTE);
-void U0_TX_BYTE_float(float data,int Dp);
-void U0_TX_BYTE_hex(int data);
 char U0_RX_BYTE(void);
 void uart0_isr(void) __irq;
 void enable_uart0(void);
